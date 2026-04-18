2bc2c005388ac75d8a1b67dda95010f8c279447b
AlperCna <alpercn04@gmail.com>
2026-04-18 19:35:07 +0300

    Video 4 Making first view

--- server/app/controllers/TaskList1.scala
+++ server/app/controllers/TaskList1.scala
@@ -9,6 +9,7 @@ import play.api.i18n._
 @Singleton
 class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(cc){
    def taskList = Action {
-     Ok("This works!")
+     val tasks = List("task1","task2","task3","sleep","eat")
+     Ok(views.html.taskList1(tasks))
    }
 }
