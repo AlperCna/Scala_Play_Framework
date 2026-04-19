2557761698896d1ae353e67eb3de09085c9d458d
AlperCna <alpercn04@gmail.com>
2026-04-19 17:24:13 +0300

    Video 13 Flash Scope

--- server/app/controllers/TaskList1.scala
+++ server/app/controllers/TaskList1.scala
@@ -18,7 +18,7 @@ class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(c
     Ok(s"$username logged in with $password .")
   }
 
-  def validateLoginPost = Action { request =>
+  def validateLoginPost = Action { implicit request  =>
    val  postVals = request.body.asFormUrlEncoded
     postVals.map {  args =>
       val username = args("username").head
@@ -26,7 +26,7 @@ class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(c
       if (TaskListInMemoryModel.validateUser(username, password)){
         Redirect(routes.TaskList1.taskList()).withSession("username"-> username)
       }else{
-        Redirect(routes.TaskList1.login())
+        Redirect(routes.TaskList1.login()).flashing("error" ->"Invalid username/password combination")
       }
 
     }.getOrElse(Redirect(routes.TaskList1.login()))
@@ -34,7 +34,7 @@ class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(c
 
 
 
-  def createUser = Action { request =>
+  def createUser = Action { implicit request =>
     val  postVals = request.body.asFormUrlEncoded
     postVals.map {  args =>
       val username = args("username").head
@@ -42,12 +42,12 @@ class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(c
       if (TaskListInMemoryModel.createUser(username, password)){
         Redirect(routes.TaskList1.taskList()).withSession("username"-> username)
       }else{
-        Redirect(routes.TaskList1.login())
+        Redirect(routes.TaskList1.login()).flashing("error" ->"User creation failed")
       }
 
     }.getOrElse(Redirect(routes.TaskList1.login()))
   }
-   def taskList = Action { request =>
+   def taskList = Action { implicit request =>
      val usernameOption = request.session.get("username")
      usernameOption.map { username =>
        val tasks = TaskListInMemoryModel.getTasks(username)
