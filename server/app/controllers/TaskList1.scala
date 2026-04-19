5f2107b5f6e7e86f848966050cbf384b459b4813
AlperCna <alpercn04@gmail.com>
2026-04-19 12:24:21 +0300

    Video 7 form data wih post

--- server/app/controllers/TaskList1.scala
+++ server/app/controllers/TaskList1.scala
@@ -17,6 +17,17 @@ class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(c
     Ok(s"$username logged in with $password .")
   }
 
+  def validateLoginPost = Action { request =>
+   val  postVals = request.body.asFormUrlEncoded
+    postVals.map {  args =>
+      val username = args("username").head
+      val password = args("password").head
+
+      Ok(s"$username logged in with $password")
+
+    }.getOrElse(Ok("Oops"))
+  }
+
    def taskList = Action {
      val tasks = List("task1","task2","task3","sleep","eat")
      Ok(views.html.taskList1(tasks))
