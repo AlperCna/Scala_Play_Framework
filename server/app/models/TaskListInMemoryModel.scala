af6f8a533422e17367e61de10611f6d4214dfccb
AlperCna <alpercn04@gmail.com>
2026-04-19 14:06:03 +0300

    Video 10 Making a model part2

--- server/app/models/TaskListInMemoryModel.scala
+++ server/app/models/TaskListInMemoryModel.scala
@@ -4,16 +4,25 @@ import collection.mutable
 object TaskListInMemoryModel {
 
   private val users = mutable.Map[String, String]("Mark" -> "pass")
-
+  private val tasks = mutable. Map[String, List[String]]("Mark" -> List("Make videos", "eat", "sleep","code"))
   def validateUser (username: String, password: String): Boolean = {
     users.get(username).map(_  == password).getOrElse(false)
 
   }
 
 
-  def createUser (username: String, password: String): Boolean = ???
+  def createUser (username: String, password: String): Boolean = {
+    if (users.contains(username)) false else {
+      users(username) = password
+      true
+    }
 
-  def getTasks(username: String): Seq [String] = ???
+  }
+
+  def getTasks(username: String): Seq [String] = {
+
+    tasks.get(username).getOrElse(Nil)
+  }
 
   def addTask(username: String, task: String): Unit = ???
 
