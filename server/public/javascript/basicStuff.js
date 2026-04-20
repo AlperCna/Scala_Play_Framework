00091f7ae1ed72caa0c75abb24f04b0b9c20fde8
AlperCna <alpercn04@gmail.com>
2026-04-20 23:09:10 +0300

    Video 27 Ajax in play part 2

--- server/public/javascript/basicStuff.js
+++ server/public/javascript/basicStuff.js
@@ -8,3 +8,19 @@ console.log("Running Js");
 $("#randomText").click(function () {
     $("#random").load("/random");
 });
+
+
+
+
+const stringText = document.getElementById("randomStringText");
+stringText.onclick = () => {
+    const lengthInput = document.getElementById("lengthValue");
+    const url = "/randomString/" + lengthInput.value;
+    console.log(url);
+    fetch(url).then((response) => {
+        return response.text();
+    }).then((responseText) => {
+        const randomString = document.getElementById("randomString");
+        randomString.innerHTML = responseText;
+    })
+};
