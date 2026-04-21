9266eb3f57587aa13a2fe1714a400bd958eaddb4
AlperCna <alpercn04@gmail.com>
2026-04-21 14:52:12 +0300

    Video 29 JQuey Ajax TaskList part 2

--- server/public/javascript/version2.js
+++ server/public/javascript/version2.js
@@ -5,3 +5,17 @@
 
 $("#contents").load("/login2")
 
+
+
+
+function login() {
+    const username = $("#loginName").val();
+    const password = $("#loginPass").val();
+    $("#contents").load("/validate2?username=" + username + "&password=" + password);
+}
+
+
+function createUser(){
+
+    console.log("Try to create user")
+}
