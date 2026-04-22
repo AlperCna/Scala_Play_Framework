b2b6ab0b21b64121b44448f9524f1c1a5a44d916
AlperCna <alpercn04@gmail.com>
2026-04-22 15:55:25 +0300

    JSON Ajax Task List Part 2

--- server/app/controllers/TaskList3.scala
+++ server/app/controllers/TaskList3.scala
@@ -5,9 +5,13 @@ import play.api.mvc._
 import play.api.i18n._
 import models.TaskListInMemoryModel
 import play.api.libs.json.Json
+import play.api.libs.json._
+import models._
 
 @Singleton
 class TaskList3 @Inject() (cc: ControllerComponents) extends AbstractController(cc) {
+
+  implicit val userDataReads = Json.reads[UserData]
 
 
   def load = Action { implicit request =>
@@ -16,6 +20,21 @@ class TaskList3 @Inject() (cc: ControllerComponents) extends AbstractController(
 
   def data = Action {
     Ok(Json.toJson(Seq("a", "b", "c")))
+  }
+
+  def validate = Action { implicit request =>
+    request.body.asJson.map { body =>
+      Json.fromJson[UserData](body) match {
+        case JsSuccess(ud, path) =>
+          if (TaskListInMemoryModel.validateUser(ud.username, ud.password)) {
+            Ok(Json.toJson(true))
+              .withSession("username" -> ud.username, "csrfToken" -> play.filters.csrf.CSRF.getToken.get.value)
+          } else {
+            Ok(views.html.login2())
+          }
+        case e @ JsError(_) => Redirect(routes.TaskList3.load())
+      }
+    }.getOrElse(Redirect(routes.TaskList3.load()))
   }
 
 
