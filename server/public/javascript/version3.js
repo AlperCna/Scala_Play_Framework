776f5de78d50d29db7e2389d74b2ef9d6eedab53
AlperCna <alpercn04@gmail.com>
2026-04-22 16:27:29 +0300

    Video 40 JSON Ajax Task List Part 3

---
+++ server/public/javascript/version3.js
@@ -0,0 +1,24 @@
+"use strict"
+
+
+const csrfToken = document.getElementById("csrfToken").value;
+const validateRoute = document.getElementById("validateRoute").value;
+const tasksRoute = document.getElementById("tasksRoute").value;
+const createRoute = document.getElementById("createRoute").value;
+const deleteRoute = document.getElementById("deleteRoute").value;
+const addRoute = document.getElementById("addRoute").value;
+const logoutRoute = document.getElementById("logoutRoute").value;
+
+
+function login() {
+    const username = document.getElementById("loginName").value;
+    const password = document.getElementById("loginPass").value;
+
+    fetch(validateRoute, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: { username, password }
+    }).then(res => res.json()).then(data => {
+        console.log(data);
+    });
+}
